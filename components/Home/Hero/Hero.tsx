a6f033fd9e5dd0cb92b9669b1f6b1d5be30de68c
David <david@MacBook-Pro-de-Service-2.local>
2025-11-09 17:27:51 +0100

    landing section Hero

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='relative h-screen items-center flex justify-center text-white overflow-hidden flex-col'>
+        Hero
+    </div>
+  )
+}
+
+export default Hero 
